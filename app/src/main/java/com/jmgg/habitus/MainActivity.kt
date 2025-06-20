04c4f124d8fa26172db9032c1f2c2f5f093cd80f
eliuthlarios <00029223@uca.edu.sv>
2025-06-20 16:52:32 -0600

    :construction: mainactivity

--- app/src/main/java/com/jmgg/habitus/MainActivity.kt
+++ app/src/main/java/com/jmgg/habitus/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.rememberNavController
+import com.jmgg.habitus.navigation.AppNavHost
 import com.jmgg.habitus.ui.theme.HabitusTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HabitusTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    AppNavHost(navController)
+                }
 
             }
         }
