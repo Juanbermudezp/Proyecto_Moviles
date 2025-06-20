61f65cb239c22df928883e61bc24c532eb2c25c1
Juanbermudezp <00218123@uca.edu.sv>
2025-06-19 21:21:12 -0600

    :memo: Base de Datos Remota

--- app/src/main/java/com/jmgg/habitus/data/datasources/remote/SupabaseHabitDataSource.kt
+++ app/src/main/java/com/jmgg/habitus/data/datasources/remote/SupabaseHabitDataSource.kt
@@ -1,4 +1,61 @@
 package com.jmgg.habitus.data.datasources.remote
 
-class SupabaseHabitDataSource {
+import com.jmgg.habitus.data.datasources.HabitDataSource
+import com.jmgg.habitus.models.Habit
+import com.jmgg.habitus.network.SupabaseApiService
+
+class SupabaseHabitDataSource(
+    private val api: SupabaseApiService,
+    private val apiKey: String
+
+) : HabitDataSource {
+
+    override suspend fun getHabitsByUser(userId: Int): List<Habit> {
+        return api.getHabits(
+            authHeader = "Bearer $apiKey",
+            apiKey = apiKey,
+            userId = "eq.$userId"
+        )
+    }
+
+    override suspend fun addHabit(habit: Habit) {
+        api.insertHabit(
+            habit = habit,
+            authHeader = "Bearer $apiKey",
+            apiKey = apiKey
+        )
+    }
+
+    override suspend fun deleteHabit(habitId: Int) {
+        val response = api.deleteHabit(
+            habitId = "eq.$habitId",
+            authHeader = "Bearer $apiKey",
+            apiKey = apiKey
+        )
+
+        if (!response.isSuccessful){
+            throw Exception("Error al eliminar habito: ${response.code()}")
+        }
+    }
+
+    override suspend fun updateHabit(habit: Habit) {
+        val response = api.updateHabit(
+            habitId = "eq.${habit.id}",
+            habit = habit,
+            authHeader = "Bearer $apiKey",
+            apiKey = apiKey
+        )
+
+        if (!response.isSuccessful) {
+            throw Exception("Error al actualizar hábito: ${response.code()}")
+        }
+    }
+
+    override suspend fun getHabitById(habitId: Int): Habit? {
+        return api.getHabitById(
+            habitId = "eq.$habitId",
+            authHeader = "Bearer $apiKey",
+            apiKey = apiKey
+        ).firstOrNull()
+    }
 }
