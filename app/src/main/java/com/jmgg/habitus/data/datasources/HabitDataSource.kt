a6fa131e98542bbe1bcff70d122af27bedaaf93c
Juanbermudezp <00218123@uca.edu.sv>
2025-06-19 21:22:03 -0600

    :memo: Interfaz de Habito

--- app/src/main/java/com/jmgg/habitus/data/datasources/HabitDataSource.kt
+++ app/src/main/java/com/jmgg/habitus/data/datasources/HabitDataSource.kt
@@ -1,4 +1,11 @@
 package com.jmgg.habitus.data.datasources
 
-interface HabitDataSource {
+import com.jmgg.habitus.models.Habit
+
+interface HabitDataSource{
+    suspend fun getHabitsByUser(userId: Int): List<Habit>
+    suspend fun addHabit(habit: Habit)
+    suspend fun deleteHabit(habitId: Int)
+    suspend fun updateHabit(habit: Habit)
+    suspend fun getHabitById(habitId: Int): Habit?
 }
