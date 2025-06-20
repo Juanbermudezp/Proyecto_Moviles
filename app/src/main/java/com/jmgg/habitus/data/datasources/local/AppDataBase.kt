ed804fef8220d5f0c32bca7d3a4e4bcbce86d722
Juanbermudezp <00218123@uca.edu.sv>
2025-06-19 21:19:07 -0600

    :memo: Base de Datos

--- app/src/main/java/com/jmgg/habitus/data/datasources/local/AppDataBase.kt
+++ app/src/main/java/com/jmgg/habitus/data/datasources/local/AppDataBase.kt
@@ -1,2 +1,15 @@
 package com.jmgg.habitus.data.datasources.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.jmgg.habitus.data.datasources.local.dao.UserDao
+import com.jmgg.habitus.data.datasources.local.entities.UserEntity
+
+@Database(
+    entities = [UserEntity::class],
+    version = 3,
+    exportSchema = false // Opcional: evita que Room genere archivos de esquema en /schemas
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
