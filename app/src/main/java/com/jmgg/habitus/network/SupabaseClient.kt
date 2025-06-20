af587e4b9494f0b6e9390c079a4c943afe476a7b
guillermodubon <00106423@uca.edu.sv>
2025-06-20 15:52:42 -0600

    :memo: SupabaseClient

--- app/src/main/java/com/jmgg/habitus/network/SupabaseClient.kt
+++ app/src/main/java/com/jmgg/habitus/network/SupabaseClient.kt
@@ -1,4 +1,14 @@
 package com.jmgg.habitus.network
 
-object SupabaseClient {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object SupabaseClient{
+
+    private const val BASE_URl = "https://zgunsowtbtynmeffqati.supabase.co/rest/v1/"
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
